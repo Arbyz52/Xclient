4c19440880b76d0beb39102008c4878b1bb46332
Arbyz52 <2korytko.artem@mail.ru>
2025-11-25 16:30:46 +0300

    Update 1.lua

--- modules/Visuals/1.lua
+++ modules/Visuals/1.lua
@@ -1,1 +1,28 @@
+local module = {
+    Name     = "ESP",
+    Category = "Visuals",
+    Enabled  = false,
+}
 
+-- вызывать один раз при загрузке, тут можно подготовить объекты/таблицы
+function module:Init()
+    print("[Xclient][ESP] Init()")
+    -- сюда потом добавишь свои переменные / кэш
+end
+
+function module:OnEnable()
+    print("[Xclient][ESP] Включен")
+    -- включение твоего ESP
+end
+
+function module:OnDisable()
+    print("[Xclient][ESP] Выключен")
+    -- выключение / очистка ESP
+end
+
+function module:OnTick(dt)
+    if not self.Enabled then return end
+    -- здесь будет твоя логика ESP, которая выполняется каждый кадр
+end
+
+return module
