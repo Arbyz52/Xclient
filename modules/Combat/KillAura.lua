6184901835ef576b5fbe86ee4aacea5a9a6bd43a
Arbyz52 <2korytko.artem@mail.ru>
2025-11-25 09:07:07 +0300

    Update KillAura.lua

--- modules/Combat/KillAura.lua
+++ modules/Combat/KillAura.lua
@@ -1,1 +1,25 @@
 
+local module = {
+    Name = "KillAura",
+    Category = "Combat", -- можно не писать, возьмётся из папки
+    Enabled = false,
+}
+
+function module:Init()
+    -- один раз при загрузке
+end
+
+function module:OnEnable()
+    -- включили
+end
+
+function module:OnDisable()
+    -- выключили
+end
+
+function module:OnTick(dt)
+    if not self.Enabled then return end
+    -- основной код твоей фичи
+end
+
+return module
