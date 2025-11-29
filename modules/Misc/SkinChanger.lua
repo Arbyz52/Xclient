b6ee03239c2b022bbc4683567746656cd5590e0b
Arbyz52 <2korytko.artem@mail.ru>
2025-11-29 18:37:30 +0300

    Update SkinChanger.lua

--- modules/Misc/SkinChanger.lua
+++ modules/Misc/SkinChanger.lua
@@ -1,4 +1,4 @@
--- modules/Misc/SkinChanger.lua
+
 local module = {
     Name     = "SkinChanger",
     Category = "Misc",
@@ -7,21 +7,21 @@ local module = {
 
 function module:OnEnable()
     self.Enabled = true
-    -- загрузка внешнего скрипта
+
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MMoonDzn/AuroraChanger/refs/heads/main/loader.lua"))()
 end
 
 function module:OnDisable()
     self.Enabled = false
-    -- при отключении можно добавить очистку, если нужно
+
 end
 
 function module:Init()
-    -- инициализация, если потребуется
+
 end
 
 function module:OnTick(dt)
-    -- тик, если нужно что-то обновлять
+
 end
 
 return module
